92b1ba786fd59f46d2317f394ac3349bf7dd0829
Ahmad Hatziq <54108059+AhmadHatziq@users.noreply.github.com>
2022-05-31 17:56:27 +0800

    add anchors to link list to wiki entries and add error page for wrong entry terms

--- Project 1 - Wiki/wiki/encyclopedia/views.py
+++ Project 1 - Wiki/wiki/encyclopedia/views.py
@@ -18,12 +18,22 @@ def display_entry(request, title):
     # Get html content of markdown file 
     html_content = util.get_entry(title)
     
-    # Create context dictionary 
-    context_dict = {
-        "html_content": html_content, 
-        "entry_name": title
-    }
+    if html_content is not None: 
     
-    return render(request, "encyclopedia/wiki_entry.html", context_dict)
+        # Create context dictionary 
+        context_dict = {
+            "html_content": html_content, 
+            "entry_name": title
+        }
+        
+        return render(request, "encyclopedia/wiki_entry.html", context_dict)
+        
+    else: 
+        # Display error page 
+        print(f"Error for {title}")
+        return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries(), 
+        "message": f"Specified entry for '{title}' does not exist. Please choose from the list below."
+    })
     
 
