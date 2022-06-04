e370479c4b12846c2efd9bf0ed83acd84fd040a1
Ahmad Hatziq <54108059+AhmadHatziq@users.noreply.github.com>
2022-06-04 17:56:52 +0800

    Minor changes

--- Project 1 - Wiki/wiki/encyclopedia/urls.py
+++ Project 1 - Wiki/wiki/encyclopedia/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = "encyclopedia"
 urlpatterns = [
     path("", views.index, name="index"), 
     path("search", views.search, name="search"),
--- Project 1 - Wiki/wiki/encyclopedia/views.py
+++ Project 1 - Wiki/wiki/encyclopedia/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from . import util
 
 import random 
