c955046c3aa0968c28d487f5ed7df349848f5aab
Ahmad Hatziq <54108059+AhmadHatziq@users.noreply.github.com>
2022-06-06 23:49:48 +0800

    Added basic functionality for adding a new listing

--- Project 2 - Commerce/commerce/auctions/urls.py
+++ Project 2 - Commerce/commerce/auctions/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("create_listing", views.create_listing, name="create_listing"), 
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register")
--- Project 2 - Commerce/commerce/auctions/views.py
+++ Project 2 - Commerce/commerce/auctions/views.py
@@ -3,9 +3,15 @@ from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django import forms 
 
 from .models import User
 
+class CreateListingForm(forms.Form): 
+    listing_name = forms.CharField(label="Listing name", widget=forms.TextInput(attrs={'class':'form-control'}))
+    listing_start_price = forms.DecimalField(label="Starting price", max_digits=9, decimal_places=2, widget=forms.TextInput(attrs={'class':'form-control'}))
+    listing_url = forms.URLField(label="Image URL", max_length=256, widget=forms.TextInput(attrs={'class':'form-control'}))
+    listing_desc = forms.CharField(label="Details", widget=forms.Textarea(attrs={'name':'body', 'rows':'3', 'cols':'5', 'class':'form-control'}))
 
 def index(request):
     return render(request, "auctions/index.html")
@@ -61,3 +67,39 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
+
+def create_listing(request): 
+    
+    # Checks for user name. Redirects back to login if user is not logged in. 
+    if request.user.is_authenticated == False: 
+        return render(request, "auctions/index.html", {
+            "message": "Please login"
+        })
+        
+    # Returns form template if method is by GET 
+    if request.method == "GET":
+        return render(request, "auctions/create_listing.html", {
+            "form": CreateListingForm()
+            })
+     
+    # Handle data sent from the form 
+    if request.method == "POST": 
+        
+        # Check if form is valid and process data 
+        form = CreateListingForm(request.POST)
+        if form.is_valid(): 
+            
+            # print(form.cleaned_data)
+            
+            # Extract data from form 
+            listing_name = form.cleaned_data['listing_name']
+            listing_start_price = form.cleaned_data['listing_start_price']
+            listing_image_url = form.cleaned_data['listing_url']
+            listing_desc = form.cleaned_data['listing_desc']
+            
+            
+            
+            
+    
+    # Return to index otherwise
+    return render(request, "auctions/index.html")
