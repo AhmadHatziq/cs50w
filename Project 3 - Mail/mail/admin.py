0c447aeffc8b43bfc26ab23adfc58e36d0918960
Ahmad Hatziq <54108059+AhmadHatziq@users.noreply.github.com>
2022-06-30 10:27:57 +0800

    Registered models for User & Email with admin site

--- Project 3 - Mail/mail/admin.py
+++ Project 3 - Mail/mail/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Email, User 
 
-# Register your models here.
+# Create the admin classes to view the details in the admin interface ------------------------------------------------
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("id", "username", "email", "password", "last_login", "date_joined")
+
+class EmailAdmin(admin.ModelAdmin): 
+    list_display = ("id", "user", "sender", "subject", "body", "timestamp", "read", "archived")
+
+# Register your models here. -----------------------------------------------------------------------------------------
+admin.site.register(User, UserAdmin)
+admin.site.register(Email, EmailAdmin)
+
