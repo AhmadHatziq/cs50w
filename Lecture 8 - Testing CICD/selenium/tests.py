52a62bcda82aefc4ad8df345ceaba3e4cce8b85f
Ahmad Hatziq <54108059+AhmadHatziq@users.noreply.github.com>
2022-07-18 16:37:33 +0800

    Fix selenium test cases
    Selenium depracated older functions. Had to update to get it working.

--- Lecture 8 - Testing CICD/selenium/tests.py
+++ Lecture 8 - Testing CICD/selenium/tests.py
@@ -4,16 +4,14 @@ import unittest
 
 from selenium import webdriver
 
-from webdriver_manager.chrome import ChromeDriverManager
-
-driver = webdriver.Chrome(ChromeDriverManager().install())
+from selenium.webdriver.common.by import By
 
 def file_uri(filename):
     return pathlib.Path(os.path.abspath(filename)).as_uri()
 
+PATH = "C:\Program Files (x86)\chromedriver.exe"
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
-
+driver = webdriver.Chrome(PATH)
 
 class WebpageTests(unittest.TestCase):
 
@@ -23,23 +21,22 @@ class WebpageTests(unittest.TestCase):
 
     def test_increase(self):
         driver.get(file_uri("counter.html"))
-        increase = driver.find_element_by_id("increase")
+        increase = driver.find_element("id", "increase")
         increase.click()
-        self.assertEqual(driver.find_element_by_tag_name("h1").text, "1")
+        self.assertEqual(driver.find_element(By.TAG_NAME, "h1").text, "1") # Assert 2 equal strings, not ints. 
 
     def test_decrease(self):
         driver.get(file_uri("counter.html"))
-        decrease = driver.find_element_by_id("decrease")
+        decrease = driver.find_element("id", "decrease")
         decrease.click()
-        self.assertEqual(driver.find_element_by_tag_name("h1").text, "-1")
+        self.assertEqual(driver.find_element(By.TAG_NAME, "h1").text, "-1")
 
     def test_multiple_increase(self):
         driver.get(file_uri("counter.html"))
-        increase = driver.find_element_by_id("increase")
+        increase = driver.find_element("id", "increase")
         for i in range(3):
             increase.click()
-        self.assertEqual(driver.find_element_by_tag_name("h1").text, "3")
-
+        self.assertEqual(driver.find_element(By.TAG_NAME, "h1").text, "3")
 
 if __name__ == "__main__":
     unittest.main()
