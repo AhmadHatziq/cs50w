406bb50118bd2c5968a184fb242f8e724012b77f
Ahmad Hatziq <54108059+AhmadHatziq@users.noreply.github.com>
2022-07-21 21:56:22 +0800

    Added User Model to admin UI

--- Project 4 - Network/project4/network/admin.py
+++ Project 4 - Network/project4/network/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User 
+
+class UserAdmin(admin.ModelAdmin): 
+    list_display = ("id", "username", "email", "password", "last_login", "date_joined", "is_staff")
 
 # Register your models here.
+admin.site.register(User, UserAdmin)
--- Project 4 - Network/project4/network/models.py
+++ Project 4 - Network/project4/network/models.py
@@ -3,4 +3,9 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    
+    users_following = models.ManyToManyField("self", blank=True, related_name='following')
+    users_follower = models.ManyToManyField("self", blank=True, related_name='follower')
+
+    def __str__(self): 
+        return f"{self.username}"
