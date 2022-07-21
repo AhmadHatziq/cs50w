bde60ff3908adfc936e03704abb50e06ded5a2ec
Ahmad Hatziq <54108059+AhmadHatziq@users.noreply.github.com>
2022-07-21 22:59:58 +0800

    Edited related_name of the FollowRelation

--- Project 4 - Network/project4/network/models.py
+++ Project 4 - Network/project4/network/models.py
@@ -7,6 +7,9 @@ class User(AbstractUser):
     def __str__(self): 
         return f"{self.username}"
 
-class FollowRelation(AbstractUser): 
-    follower = models.ForeignKey(User, on_delete = models.CASCADE, related_name = 'follower')
+class FollowRelation(models.Model): 
+    follower = models.ForeignKey(User, on_delete = models.CASCADE, related_name = 'follower_user')
     users_following = models.ManyToManyField(User, blank = True, related_name = 'users_following')
+
+    def __str__(self):   
+        return f"{self.follower.username}"
