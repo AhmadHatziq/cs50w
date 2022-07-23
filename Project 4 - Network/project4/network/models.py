42db9f77750c4f3e46475130091e9f32cd0dc9d0
Ahmad Hatziq <54108059+AhmadHatziq@users.noreply.github.com>
2022-07-23 20:28:35 +0800

    Add models for Post & Like. Edit FollowRelation model

--- Project 4 - Network/project4/network/models.py
+++ Project 4 - Network/project4/network/models.py
@@ -3,13 +3,38 @@ from django.db import models
 
 
 class User(AbstractUser):
-
+    """
+    Represents a single User. 
+    """
     def __str__(self): 
         return f"{self.username}"
 
+
 class FollowRelation(models.Model): 
+    """
+    Represents the single relationship of a User following other User(s). 
+    There will always be a single FollowRelation for each User. 
+    When a User follows a new User, a FollowRelation is either created (for the first follow) or edited & appended. 
+    """
     follower = models.ForeignKey(User, on_delete = models.CASCADE, related_name = 'follower_user')
     users_following = models.ManyToManyField(User, blank = True, related_name = 'users_following')
 
     def __str__(self):   
         return f"{self.follower.username}"
+
+class Post(models.Model): 
+    """
+    Represents a single textual post created by a user. 
+    """
+    post_text_content = models.CharField(max_length=255)
+    post_user =  models.ForeignKey(User, on_delete = models.CASCADE, related_name = 'comment_poster')
+
+class Like(models.Model): 
+    """
+    Represents a Like of a User for a Post. 
+    """
+    liked_post = models.ForeignKey(Post, on_delete = models.CASCADE) 
+    liked_by = models.ForeignKey(User, on_delete = models.CASCADE)
+
+
+
