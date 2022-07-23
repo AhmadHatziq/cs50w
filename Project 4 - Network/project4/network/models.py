ce02aa421034c7c48acad3f12001da34bbb1589e
Ahmad Hatziq <54108059+AhmadHatziq@users.noreply.github.com>
2022-07-23 23:03:13 +0800

    Add workaround for displaying a ManyToManyField in admin UI
    See: https://stackoverflow.com/questions/18108521/many-to-many-in-list-display-django

--- Project 4 - Network/project4/network/models.py
+++ Project 4 - Network/project4/network/models.py
@@ -15,12 +15,18 @@ class FollowRelation(models.Model):
     Represents the single relationship of a User following other User(s). 
     There will always be a single FollowRelation for each User. 
     When a User follows a new User, a FollowRelation is either created (for the first follow) or edited & appended. 
+    If A is following B & C, there will be a single entry where: A -> (B, C)
+        follower: A 
+        users_following: B, C 
     """
     follower = models.ForeignKey(User, on_delete = models.CASCADE, related_name = 'follower_user')
     users_following = models.ManyToManyField(User, blank = True, related_name = 'users_following')
 
     def __str__(self):   
         return f"{self.follower.username}"
+
+    def get_users_being_followed(self): 
+        return "\n".join([u.username for u in self.users_following.all()])
 
 class Post(models.Model): 
     """
