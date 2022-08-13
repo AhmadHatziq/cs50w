0a19d171fc89568745b71a9cb015c08f9352549e
Ahmad Hatziq <54108059+AhmadHatziq@users.noreply.github.com>
2022-08-13 18:33:11 +0800

    Added a legend and parsed the Python using JSON dumps, which is sent to JS
    See https://stackoverflow.com/questions/4162642/single-vs-double-quotes-in-json
    for the sending of Python list to JavaScript using JSON.
    Take note of the usage of single and double quotations.

--- Project 5 - Capstone/capstone/geocache/views.py
+++ Project 5 - Capstone/capstone/geocache/views.py
@@ -8,6 +8,7 @@ from django.urls import reverse
 from . import utils
 from .models import User, Geocache, DiscussionBoard 
 
+import json
 
 API_KEY = utils.load_google_maps_API_key()
 
@@ -73,15 +74,81 @@ def view_map(request):
     '''
     Renders the map as well as geocaches (solved and not solved). 
     '''
+
+    # Extracts the Google maps API key out. 
     context_dict = {
         'API_KEY': API_KEY
     }
 
     # Checks if banner message exists and add it to the context dict if so. 
+    # Used when redirecting after user has submitted a geocache. 
     if 'banner_message' in request.session: 
         banner_message = request.session['banner_message']
         del request.session['banner_message']
         context_dict['message'] = banner_message
+
+    # Extract the geocache data out. 
+    goecache_posts = Geocache.objects.all() 
+    current_user = User.objects.get(username=request.user.username)
+
+    # Process the geocache posts and check if: 
+    #   (i)     the current user is the owner (user pin will show a different color)
+    #   (ii)    that post has been found yet by the user
+    processed_geoposts = []
+    for geocache_post in goecache_posts: 
+
+        # Check if the user logged in is the poster of this particular post. 
+        is_owner = False
+        if geocache_post.poster == current_user: 
+            is_owner = True 
+
+        # Check if the current user has found the geocache. Owners cannot find their own geocaches. 
+        is_founder = False 
+        founders = geocache_post.founder.all() 
+        if current_user in founders: 
+            is_founder = True 
+
+        # Determine which icon to use based on is_owner and is_founder. 
+        STATIC_ICON_DIRECTORY = r'/static/geocache/images/'
+        OWNER_ICON, UNFOUND_ICON, FOUND_ICON = 'blue_map_icon.png', 'blue_map_icon.png', 'red_map_icon.png' 
+        icon_location = ''
+        if is_owner: 
+            icon_location = STATIC_ICON_DIRECTORY + OWNER_ICON
+        else: 
+            if is_founder: 
+                icon_location = STATIC_ICON_DIRECTORY + FOUND_ICON 
+            else: 
+                icon_location = STATIC_ICON_DIRECTORY + UNFOUND_ICON
+
+        # Check if the current user has added this geocache to their watchlist. 
+        in_watchlist = False
+        watchlist_users = geocache_post.users_following.all() 
+        if current_user in watchlist_users: 
+            in_watchlist = True 
+
+        # Store fields in a new dictionary and append to the list 
+        single_geocache = {
+            'is_owner': str(is_owner), 
+            'is_founder': str(is_founder), 
+            'in_watchlist': str(in_watchlist),
+            'icon_location': icon_location,
+            'latitude': str(geocache_post.latitude), 
+            'longitude': str(geocache_post.longitude), 
+            'hint': geocache_post.hint, 
+            'id': str(geocache_post.id)
+        }
+        processed_geoposts.append(single_geocache)
+
+    # Add processed geoposts to context dictionary. 
+    # Convert to json dumps so that JavaScript can recreate the object. 
+    # We use the json library as in JavaScript, strings have double quotes. 
+    # But in Python, it can be single quotes (which messes up JavaScript's JSON parser). 
+    # Taken from https://stackoverflow.com/questions/4162642/single-vs-double-quotes-in-json
+
+
+    context_dict['geocaches'] = json.dumps(processed_geoposts)
+
+
 
     return render(request, 'geocache/main_map.html', context_dict)
 
