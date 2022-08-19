cf2c000f14e8dc1d4fda232c6fffb12c08b041bd
Ahmad Hatziq <54108059+AhmadHatziq@users.noreply.github.com>
2022-08-19 20:54:15 +0800

    Edit utils.py to find API key only at files within a folder called 'api_key'

--- Project 5 - Capstone/capstone/geocache/utils.py
+++ Project 5 - Capstone/capstone/geocache/utils.py
@@ -3,13 +3,21 @@ import os
 def load_google_maps_API_key(): 
     '''
     Loads the Google Maps API key from the local directory. 
+    Looks for either the file called 'GOOGLE_MAPS_API.txt' or 'google_maps_api_key.txt'. 
     '''
 
     current_directory = os.path.dirname(__file__)
-    target_file = os.path.join(current_directory, 'api_key', 'GOOGLE_MAPS_API.txt')
+    api_folder = os.path.join(current_directory, 'api_key')
 
-    print(target_file)
-    with open(target_file, 'r') as f: 
-        extracted_api_key = f.read() 
-        print('LOADED GOOGLE MAPS API KEY:', extracted_api_key, '\n')
-        return extracted_api_key
+    for file in os.listdir(api_folder): 
+        os.chdir(api_folder)
+        with open(file, 'r') as f: 
+            extracted_api_key = f.read() 
+            if len(extracted_api_key) > 1 and 'replace' not in extracted_api_key.lower(): 
+                print('LOADED GOOGLE MAPS API KEY:', extracted_api_key, '\n')
+                return extracted_api_key
+
+    # Print error message as no API key found. 
+    print('UNABLE TO FIND ANY API KEY STRINGS in: ', api_folder)
+    quit()
+    
